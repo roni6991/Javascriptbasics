ba0ff50d7617dfcc1ff371ffba70b2f496c37fca
Roni <nazmlroni@gmail.com>
2021-01-03 23:15:13 +0600

    jsb

---
+++ constlet.js
@@ -0,0 +1,11 @@
+var name = 'Junaidul';
+const country = ('Rosie');
+// country='Cheyoung';
+if(name.length>6)
+console.log('length too big');
+else 
+console.log(name);
+console.log(country);
+let love =('rose');
+love ='100times';
+console.log(love);
